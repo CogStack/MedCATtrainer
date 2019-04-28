fe47299a3d35020a8b95f94e8f4b86e368337919
w-is-h <w.kraljevic@gmail.com>
2019-04-28 12:34:27 +0000

    Reorg

--- webapp/core/urls.py
+++ webapp/core/urls.py
@@ -1,4 +1,4 @@
-"""cattrainer URL Configuration
+"""core URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
--- webapp/core/wsgi.py
+++ webapp/core/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for cattrainer project.
+WSGI config for core project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cattrainer.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
