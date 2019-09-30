f6a1f97b75ce1c2b77b7446edbba50f3fdd1cab1
w-is-h <w.kraljevic@gmail.com>
2019-09-30 14:15:17 +0000

    Fix timeout

--- webapp/run.sh
+++ webapp/run.sh
@@ -8,4 +8,4 @@ python /home/api/manage.py migrate --noinput
 python /home/api/manage.py migrate api --noinput
 
 python /home/api/manage.py process_tasks &
-uwsgi --http :8000 --master --chdir /home/api/ --module core.wsgi
+uwsgi --http-timeout 360s --http :8000 --master --chdir /home/api/  --module core.wsgi
