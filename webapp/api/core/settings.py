6fc61f39aa43da896de2fde354ea75f10f438cec
w-is-h <w.kraljevic@gmail.com>
2019-10-18 12:18:50 +0000

    Fix for login

--- webapp/api/core/settings.py
+++ webapp/api/core/settings.py
@@ -113,8 +113,8 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 30,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+#        'rest_framework.authentication.BasicAuthentication',
+#        'rest_framework.authentication.SessionAuthentication',
     ]
 }
 
