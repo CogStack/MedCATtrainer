7b78c1c3b481f033c21614e4bd989faf070ef4df
tomolopolis <tsearle88@gmail.com>
2019-10-30 17:43:26 +0000

    use localhost

--- webapp/frontend/vue.config.js
+++ webapp/frontend/vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
   assetsDir: 'static', // For simple configuration of static files in Flask (the "static_folder='client/dist/static'" part in app.py)
   // needs to change for django-rest to serve up final built files.
   devServer: {
-    proxy: 'http://10.200.104.228:8001' // hardcoded to a rosalind ip right now. Config up later.
+    proxy: 'http://localhost:8001'
   },
   configureWebpack: {
     devtool: 'source-map'
