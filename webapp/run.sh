5d7a0acc1efcad39005e53d60f7c8a6ecade8196
tomolopolis <tsearle88@gmail.com>
2020-04-01 15:40:07 +0100

    create a default admin user upon startup if one doesn't exist

--- webapp/run.sh
+++ webapp/run.sh
@@ -8,4 +8,12 @@ python /home/api/manage.py migrate --noinput
 python /home/api/manage.py migrate api --noinput
 
 python /home/api/manage.py process_tasks &
+
+# create a new super user, with username and password 'admin'
+echo "from django.contrib.auth import get_user_model
+User = get_user_model()
+if User.objects.count() == 0:
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin')
+" | python manage.py shell
+
 uwsgi --http-timeout 360s --http :8000 --master --chdir /home/api/  --module core.wsgi
