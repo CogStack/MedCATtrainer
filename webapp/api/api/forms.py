34ee393e24a237c34b2643680d41f93642331253
tomolopolis <tsearle88@gmail.com>
2021-10-21 12:10:37 +0100

    CATTRAINER-42: remove dataset file when model is deleted

--- webapp/api/api/forms.py
+++ webapp/api/api/forms.py
@@ -1,10 +1,22 @@
-from django.db.models.signals import post_save
+import os
+
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
-from .data_utils import *
+from api.data_utils import dataset_from_file
+from api.models import Dataset
 
 
-# Extract text from the uploaded dataset
 @receiver(post_save, sender=Dataset)
 def save_dataset(sender, instance, **kwargs):
-    text_from_csv(instance)
+    dataset_from_file(instance)
+
+
+@receiver(post_delete, sender=Dataset)
+def remove_dataset_file(sender, instance, **kwargs):
+    """
+    Removes corresponding file from Dataset.original_file FileField.
+    """
+    if instance.original_file:
+        if os.path.isfile(instance.original_file.path):
+            os.remove(instance.original_file.path)
