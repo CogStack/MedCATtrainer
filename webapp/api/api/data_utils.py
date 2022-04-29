6367e6730b08e0ee2cf3ab0aafe06bf1209ce10e
tomolopolis <tsearle88@gmail.com>
2022-04-29 13:54:01 +0100

    CU-29107bz: configurable max dataset size

--- webapp/api/api/data_utils.py
+++ webapp/api/api/data_utils.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 from .models import *
-import numpy as np
 
+_MAX_DATASET_SIZE_DEFAULT = 10000
 
 def dataset_from_file(dataset: Dataset):
     df = None
@@ -12,9 +12,17 @@ def dataset_from_file(dataset: Dataset):
         df = pd.read_excel(dataset.original_file.path)
 
     df.columns = [c.lower() for c in df.columns]
+
     if df is not None:
-        if 'text' not in df.columns:
-            raise Exception("Please make sure the uploaded file has a column with header 'text'")
+        max_dataset_size = os.environ.get('MAX_DATASET_SIZE', _MAX_DATASET_SIZE_DEFAULT)
+        if df.shape[0] > max_dataset_size:
+            raise(f'Attempting to upload a dataset with {df.shape[0]} rows. The Max dataset size is set to'
+                  f' {max_dataset_size}, please reduce the number of rows or contact an admin to increase the max size')
+
+        if 'text' not in df.columns or 'name' not in df.columns:
+            raise Exception("Please make sure the uploaded file has a column with two columns:'name', 'text'. "
+                            "The 'name' column are document IDs, and the 'text' column is the text you're "
+                            "collecting annotations for")
 
         for i, row in enumerate(df.iterrows()):
             row = row[1]
