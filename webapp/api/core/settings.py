9de89c513ad4463e753697e539e2e31c5c807ac6
tomolopolis <tsearle88@gmail.com>
2022-05-27 13:55:07 +0100

    CU-2d4qw7y: bump default page size to be higher

--- webapp/api/core/settings.py
+++ webapp/api/core/settings.py
@@ -145,7 +145,7 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 30,
+    'PAGE_SIZE': 100,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication'
     ]
