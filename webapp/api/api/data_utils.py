9103eab8f1c1676e91ff150029bebab61eddc832
tomolopolis <tsearle88@gmail.com>
2022-12-15 19:27:36 +0000

    CU-2x7p3hp: error out on non unique names by default. also re-organised the compose .env file

--- webapp/api/api/data_utils.py
+++ webapp/api/api/data_utils.py
@@ -17,6 +17,10 @@ def dataset_from_file(dataset: Dataset):
 
     df.columns = [c.lower() for c in df.columns]
     max_dataset_size = os.environ.get('MAX_DATASET_SIZE', _MAX_DATASET_SIZE_DEFAULT)
+
+    if df['name'].nunique() != df.shape[0] and os.environ.get('UNIQUE_DOC_NAMES_IN_DATASETS', True):
+        raise Exception('name column entries must be unique')
+
     if df.shape[0] > int(max_dataset_size):
         raise Exception(f'Attempting to upload a dataset with {df.shape[0]} rows. The Max dataset size is set to'
                         f' {max_dataset_size}, please reduce the number of rows or contact the MedCATTrainer'
@@ -26,6 +30,7 @@ def dataset_from_file(dataset: Dataset):
         raise Exception("Please make sure the uploaded file has a column with two columns:'name', 'text'. "
                         "The 'name' column are document IDs, and the 'text' column is the text you're "
                         "collecting annotations for")
+
 
     for i, row in enumerate(df.iterrows()):
         row = row[1]
