e2cd032b096b91d66f94eff7ae04e826d842b842
tomolopolis <tsearle88@gmail.com>
2023-05-15 15:43:39 +0100

    another interim commit

--- webapp/api/core/urls.py
+++ webapp/api/core/urls.py
@@ -68,5 +68,6 @@ urlpatterns = [
     path('api/generate-concept-filter/', api.views.generate_concept_filter),
     path('api/metrics/', api.views.metrics),
     path('api/concept-path/', api.views.cdb_concept_path),
+    path('api/generate-concept-filter/', api.views.generate_concept_filter),
     re_path('^.*$', api.views.index, name='index'),  # Match everything else to home
 ]
