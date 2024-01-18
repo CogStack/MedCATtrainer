7277ee34256281f516fc11fb451f18a9e8755535
adam-sutton-1992 <adam.sutton@kcl.ac.uk>
2024-01-18 17:17:14 +0000

    added custom email message

--- webapp/api/core/settings.py
+++ webapp/api/core/settings.py
@@ -75,6 +75,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, "..", "frontend", "dist"),
+            os.path.join(BASE_DIR, "..", "templates", "registration")
             ],
         'APP_DIRS': True,
         'OPTIONS': {
