8093d3d724bd9ecf64715966375e65d420115c2c
Tom Searle <tsearle88@gmail.com>
2024-11-28 17:36:08 +0000

    CU-8696ent8k: increase timeout, and ensure database locked errors do not occur during DB reads and bg_process tasks are writing to he DB.

--- webapp/api/core/settings.py
+++ webapp/api/core/settings.py
@@ -130,6 +130,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db/db.sqlite3'),
+        'OPTIONS': {
+            'timeout': 20,
+            'transaction_mode': 'IMMEDIATE'
+        }
     }
 }
 
